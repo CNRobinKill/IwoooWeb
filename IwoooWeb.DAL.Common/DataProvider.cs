ed7893ce41d4a12ab500bfdd8f1b6791a26236ea
RobinZhang <yuebin20082008@live.com>
2013-09-05 03:10:03 +0800

    初级1
    初级1

---
+++ IwoooWeb.DAL.Common/DataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace IwoooWeb.DAL.Common
+{
+    /// <summary>
+    /// 数据库连接字符串
+    /// </summary>
+    public class DataProvider
+    {
+        private const string ConStr = "server=.;Database=IwoooWeb;User ID=sa; Password=1q2w3e4r";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConStr);
+        }
+    }
+}
---
+++ IwoooWeb.DAL.Common/SqlHelper.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IwoooWeb.DAL.Common
+{
+
+    /// <summary>
+    /// 数据库通用逻辑组件
+    /// </summary>
+    public class SqlHelper
+    {
+        /// <summary>
+        /// 多行查询（返回所有行）
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static DataSet ExecuteDataSet(string spName, SqlParameter[] paras)
+        {
+            DataSet ds = new DataSet();
+            using(SqlConnection con= DataProvider.GetConnection())
+            {
+                using (SqlCommand com=new SqlCommand())
+                {
+                    SqlDataAdapter ad=new SqlDataAdapter();
+                    com.Connection = con;
+                    ad.SelectCommand = com;
+                    com.CommandText = spName;
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.CommandTimeout = 300;
+                    if (paras != null)
+                    {
+                        com.Parameters.AddRange(paras);
+                    }
+                    con.Open();
+                    ad.Fill(ds);
+                    con.Close();
+                }
+            }
+            return ds;
+        }
+
+        /// <summary>
+        /// 单行查询（返回第一行）
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static SqlDataReader ExecuteReader(string spName, SqlParameter[] paras)
+        {
+            using (SqlConnection con = DataProvider.GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.Connection = con;
+                    com.CommandText = spName;
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.CommandTimeout = 300;
+                    if (paras != null)
+                    {
+                        com.Parameters.AddRange(paras);
+                    }
+                    con.Open();
+                    SqlDataReader reader = com.ExecuteReader(CommandBehavior.CloseConnection);
+                    return reader;
+                }
+            }          
+        }
+
+        /// <summary>
+        /// 单值查询（返回第一行第一列）
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string spName, SqlParameter[] paras)
+        {
+            object objectValue;
+            using (SqlConnection con = DataProvider.GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.Connection = con;
+                    com.CommandText = spName;
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.CommandTimeout = 300;
+                    if (paras != null)
+                    {
+                        com.Parameters.AddRange(paras);
+                    }
+                    con.Open();
+                    objectValue = com.ExecuteScalar();
+                    con.Close();
+                }              
+            }
+            return objectValue;
+        }
+
+        /// <summary>
+        /// 非查询（插入，修改，删除）
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string spName, SqlParameter[] paras)
+        {
+            int rowsAffected;
+            using (SqlConnection con = DataProvider.GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.Connection = con;
+                    com.CommandText = spName;
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.CommandTimeout = 300;
+                    if (paras != null)
+                    {
+                        com.Parameters.AddRange(paras);
+                    }
+                    con.Open();
+                    rowsAffected = com.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            return rowsAffected;
+        }
+    }
+}
